3ceeae503c498774c802697235203bf97703c8fa
zhwang <zhwang@i9i8.com>
2018-04-10 15:17:38 +0800

    添加字符串与date转换方法

--- base/Classes/DateFormatManger.h
+++ base/Classes/DateFormatManger.h
@@ -10,8 +10,16 @@
 @interface DateFormatManger : NSObject
 
 + (NSDateFormatter *)dateFormatterForStyle:(NSDateFormatterStyle)style;
++ (NSDateFormatter *)dateFormatterForFormat:(NSString *)format;
++ (NSDateFormatter *)dateFormatterForMode:(UIDatePickerMode)dateMode;
 
 + (NSString *)stringOfDate:(NSDate *)date forStyle:(NSDateFormatterStyle)style;
 + (NSDate *)dateOfString:(NSString *)string forStyle:(NSDateFormatterStyle)style;
+
++ (NSString *)stringOfDate:(NSDate *)date forFormat:(NSString *)format;
++ (NSDate *)dateOfString:(NSString *)string forFormat:(NSString *)format;
+
++ (NSString *)stringOfDate:(NSDate *)date forMode:(UIDatePickerMode)mode;
++ (NSDate *)dateOfString:(NSString *)string forMode:(UIDatePickerMode)mode;
 
 @end
--- base/Classes/DateFormatManger.m
+++ base/Classes/DateFormatManger.m
@@ -15,12 +15,55 @@
     return dateFormatter;
 }
 
++ (NSDateFormatter *)dateFormatterForFormat:(NSString *)format {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:format];
+    return dateFormatter;
+}
+
++ (NSDateFormatter *)dateFormatterForMode:(UIDatePickerMode)dateMode {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    switch (dateMode) {
+        case UIDatePickerModeTime:
+            [dateFormatter setDateFormat:@"HH:mm"];
+            break;
+        case UIDatePickerModeDate:
+            [dateFormatter setDateFormat:@"yyyy-MM-dd"];
+            break;
+        case UIDatePickerModeDateAndTime:
+            [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm"];
+            break;
+        case UIDatePickerModeCountDownTimer:
+            [dateFormatter setDateFormat:@"HH:mm"];
+            break;
+        default:
+            break;
+    }
+    return dateFormatter;
+}
+
 + (NSString *)stringOfDate:(NSDate *)date forStyle:(NSDateFormatterStyle)style {
     return [[DateFormatManger dateFormatterForStyle:style] stringFromDate:date];
 }
 
 + (NSDate *)dateOfString:(NSString *)string forStyle:(NSDateFormatterStyle)style {
     return [[DateFormatManger dateFormatterForStyle:style] dateFromString:string];
+}
+
++ (NSString *)stringOfDate:(NSDate *)date forFormat:(NSString *)format {
+    return [[DateFormatManger dateFormatterForFormat:format] stringFromDate:date];
+}
+
++ (NSDate *)dateOfString:(NSString *)string forFormat:(NSString *)format {
+    return [[DateFormatManger dateFormatterForFormat:format] dateFromString:string];
+}
+
++ (NSString *)stringOfDate:(NSDate *)date forMode:(UIDatePickerMode)mode {
+    return [[DateFormatManger dateFormatterForMode:mode] stringFromDate:date];
+}
+
++ (NSDate *)dateOfString:(NSString *)string forMode:(UIDatePickerMode)mode {
+    return [[DateFormatManger dateFormatterForMode:mode] dateFromString:string];
 }
 
 @end
