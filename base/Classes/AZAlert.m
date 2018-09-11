a4ac5278502a46230a6e02ac93fcd81f9c33c0d7
zhwang <zhwang@i9i8.com>
2018-09-11 17:51:54 +0800

    alert组件使用QMUIAlert

--- base/Classes/AZAlert.m
+++ base/Classes/AZAlert.m
@@ -18,15 +18,7 @@
 #define animate_duration 0.3
 
 @interface AZAlert ()
-@property (nonatomic, strong) UIView *contentView;
-@property (nonatomic, strong) UIView *alertView;
-@property (nonatomic, strong) UIImageView *header;
-@property (nonatomic, strong) UILabel *titleLabel;
-@property (nonatomic, strong) NSMutableArray <UIView *>*detailLabels;
-@property (nonatomic, strong) UIView *footerSeparator;
-@property (nonatomic, strong) NSMutableArray <QMUIButton *>*items;
-@property (nonatomic, assign) BOOL preferred;
-@property (nonatomic, weak) AZProgressHUD *hud;
+@property (nonatomic, strong) QMUIAlertController *QMUIAlert;
 @end
 
 @implementation AZAlert
@@ -46,167 +38,131 @@
 
 + (instancetype)alertWithTitle:(NSString *)title detailTexts:(NSArray<NSString *> *)details preferConfirm:(BOOL)preferred {
     AZAlert *alert = [AZAlert sharedInstance];
-    alert.items = [NSMutableArray array];
-    alert.preferred = preferred;
-    
-    alert.contentView = ({
-        UIView *view = [[UIView alloc] init];
-        view.backgroundColor = UIColorClear;
-        view;
-    });
-    
-    alert.alertView = ({
-        UIView *view = [[UIView alloc] init];
-        view.backgroundColor = UIColorWhite;
-        view.layer.cornerRadius = 5;
-        view.layer.masksToBounds = YES;
-        view;
-    });
-    
-    alert.header = ({
-        UIImageView *view = [UIImageView new];
-        view.contentMode = PHImageContentModeAspectFit;
-        view.hidden = YES;
-        view;
-    });
-    
-    alert.titleLabel = ({
-        UILabel *label = [[UILabel alloc] init];
-        label.text = title;
-        label.font = [UIFont dynamic_fontWithName:IS_AZAZIE?OrdinaryBold:FunctionProDemi size:IS_AZAZIE?18.f:19.f];
-        label.textColor = UIColorMakeWithHex(@"#333333");
-        label;
-    });
-    
-    alert.detailLabels = ({
-        details = details.count==0?@[@"Unknow details describtion"]:details;
-        NSMutableArray *labels = [NSMutableArray arrayWithCapacity:details.count];
-        if (details.count == 1) {
-            [labels addObject:[alert generateNormalLabelWithText:details.firstObject]];
-        } else {
-            [details enumerateObjectsUsingBlock:^(NSString * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-                [labels addObject:[alert generateNormalLabelWithText:[NSString stringWithFormat:@"•  %@",obj]]];
-            }];
-        }
-        labels;
-    });
-    
-    alert.footerSeparator = ({
-        UILabel *label = [[UILabel alloc] init];
-        label.backgroundColor = UIColorMakeWithHex(@"#dcdcdc");
-        label;
-    });
-    [alert handleConstraintsForSubViews];
+    [alert initAlertWithTitle:title messages:details];
     return alert;
 }
 
-- (void)handleConstraintsForSubViews {
-    [self.contentView addSubview:self.alertView];
-    [self.alertView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.contentView).offset(30);
-        make.left.right.bottom.equalTo(self.contentView);
-    }];
-    [self.contentView addSubview:self.header];
-    [self.header mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.equalTo(self.contentView);
-        make.top.equalTo(self.contentView);
-        make.size.mas_equalTo(CGSizeMake(60, 60));
-    }];
-    [self.alertView addSubview:self.titleLabel];
-    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_equalTo(48);
-        make.centerX.equalTo(self.alertView);
-    }];
-    NSUInteger count = self.detailLabels.count;
-    __block UIView *last = nil;
-    [self.detailLabels enumerateObjectsUsingBlock:^(UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        if (!obj.superview) {
-            [self.alertView addSubview:obj];
-        }
-        if (last) {
-            [obj mas_makeConstraints:^(MASConstraintMaker *make) {
-                make.top.equalTo(last.mas_bottom);
-                make.left.right.equalTo(last);
-            }];
-        } else {
-            UILabel *subtitle = [self generateNormalLabelWithText:[NSString stringWithFormat:@"We find %ld problems below:",count]];
-            [self.alertView addSubview:subtitle];
-            
-            [subtitle mas_makeConstraints:^(MASConstraintMaker *make) {
-                make.leading.mas_equalTo(40);
-                make.trailing.mas_equalTo(-40);
-                make.top.equalTo(self.titleLabel.mas_bottom).offset(10);
-            }];
-            subtitle.hidden = count==1;
-            [obj mas_makeConstraints:^(MASConstraintMaker *make) {
-                make.leading.trailing.equalTo(subtitle);
-                make.top.equalTo(subtitle.hidden?self.titleLabel.mas_bottom:subtitle.mas_bottom).offset(subtitle.hidden?10:0);
-            }];
-        }
-        last = obj;
-    }];
-    
-    [self.alertView addSubview:self.footerSeparator];
-    [self.footerSeparator mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo([self.detailLabels lastObject].mas_bottom).offset(32);
-        make.left.right.equalTo(self.alertView);
-        make.height.mas_equalTo(1);
-    }];
+- (void)initAlertWithTitle:(NSString *)title messages:(NSArray<NSString *> *)messages {
+    self.QMUIAlert = [[QMUIAlertController alloc] initWithTitle:title message:[self processedMessageForMessages:messages] preferredStyle:QMUIAlertControllerStyleAlert];
+    [self handleAlertControllerAttributes:self.QMUIAlert];
 }
 
-- (UILabel *)generateNormalLabelWithText:(NSString *)text {
-    UILabel *label = [[UILabel alloc] init];
-    label.font = [UIFont dynamic_fontWithName:IS_AZAZIE?Ordinary:FunctionProBook size:IS_AZAZIE?13.f:15.f];
-    label.textColor = UIColorMakeWithHex(@"#333333");
-    label.lineBreakMode = NSLineBreakByWordWrapping;
-    label.numberOfLines = 0;
-    [label setQmui_lineHeight:18];
-    label.textAlignment = NSTextAlignmentCenter;
-    label.text = text;
-    return label;
+- (void)handleAlertControllerAttributes:(QMUIAlertController *)alertController {
+    alertController.alertButtonHeight = 49.f;
+    alertController.alertContentCornerRadius = 5.f;
+    alertController.alertTitleMessageSpacing = 15.f;
+    alertController.alertHeaderBackgroundColor = UIColorWhite;
+    alertController.alertButtonBackgroundColor = UIColorWhite;
+    alertController.alertHeaderInsets = UIEdgeInsetsMake(48.f, 48.f, 32.f, 48.f);
+    alertController.alertContentMaximumWidth = [UIScreen mainScreen].bounds.size.width - 80.f;
+    
+    NSMutableDictionary *titleAttributes = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *buttonAttributes = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *messageAttributes = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *cancelButtonAttributes = [[NSMutableDictionary alloc] init];
+    
+    titleAttributes[NSFontAttributeName] = UIFontBoldMake(18.f);
+    titleAttributes[NSForegroundColorAttributeName] = UIColorMakeWithHex(@"#333333");
+    
+    buttonAttributes[NSFontAttributeName] = UIFontBoldMake(15.f);
+    buttonAttributes[NSForegroundColorAttributeName] = UIColorMakeWithHex(@"#E8437B");
+    
+    messageAttributes[NSFontAttributeName] = UIFontMake(12.f);
+    messageAttributes[NSForegroundColorAttributeName] = UIColorMakeWithHex(@"#333333");
+    messageAttributes[NSParagraphStyleAttributeName] = [NSMutableParagraphStyle qmui_paragraphStyleWithLineHeight:18.f lineBreakMode:NSLineBreakByWordWrapping];
+    
+    cancelButtonAttributes[NSFontAttributeName] = UIFontBoldMake(15.f);
+    cancelButtonAttributes[NSForegroundColorAttributeName] = UIColorMakeWithHex(@"#333333");
+    
+    alertController.alertTitleAttributes = titleAttributes;
+    alertController.alertButtonAttributes = buttonAttributes;
+    alertController.alertMessageAttributes = messageAttributes;
+    alertController.alertCancelButtonAttributes = cancelButtonAttributes;
+    
+    UIView *maskView = [self.QMUIAlert valueForKey:@"maskView"];
+    maskView.backgroundColor = UIColorMakeWithRGBA(0, 0, 0, .7f);
+}
+
+- (NSString *)processedMessageForMessages:(NSArray<NSString *> *)messages {
+    if (messages.count == 1) {
+        return messages[0];
+    }
+    if (messages.count > 1) {
+        NSString *dispalyMsg = [NSString stringWithFormat:@"We find %ld problems below:", messages.count];
+        for (NSString *message in messages) {
+            dispalyMsg = [dispalyMsg stringByAppendingString:[@"\n•  " stringByAppendingString:message]];
+        }
+        return dispalyMsg;
+    }
+    return @"Unknow details describtion";
 }
 
 - (void)addHeaderImage:(UIImage *)img {
     if (!img) {
         return;
     }
-    self.header.hidden = NO;
-    self.header.image = img;
+    UIImageView *image = [[UIImageView alloc] initWithImage:img];
+    
+    UIView *containerView = [self.QMUIAlert valueForKey:@"containerView"];
+    UIView *scrollWrapView = [self.QMUIAlert valueForKey:@"scrollWrapView"];
+    UIScrollView *headerScrollView = [self.QMUIAlert valueForKey:@"headerScrollView"];
+    
+    containerView.clipsToBounds = NO;
+    scrollWrapView.clipsToBounds = NO;
+    headerScrollView.clipsToBounds = NO;
+    
+    [headerScrollView addSubview:image];
+    [image mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(headerScrollView.mas_top);
+        make.centerX.equalTo(headerScrollView);
+    }];
+    
+    [headerScrollView bringSubviewToFront:image];
 }
 
 - (void)addCancelItemWithTitle:(NSString *)title action:(dispatch_block_t)action {
-    [self addItemWithTitleAttributes:@{NSForegroundColorAttributeName:self.preferred?UIColorMakeWithHex(@"#333333"):UIColorMakeWithHex(IS_AZAZIE?@"#E8437B":@"#E73A8F")} title:title action:action];
+    QMUIAlertAction *QMUIAction = [QMUIAlertAction actionWithTitle:title style:QMUIAlertActionStyleCancel handler:^(__kindof QMUIAlertController *aAlertController, QMUIAlertAction *qaction) {
+        if (action) {
+            action();
+        }
+    }];
+    [self.QMUIAlert addAction:QMUIAction];
 }
 
 - (void)addConfirmItemWithTitle:(NSString *)title action:(dispatch_block_t)action {
-    [self addItemWithTitleAttributes:@{NSForegroundColorAttributeName:self.preferred?UIColorMakeWithHex(IS_AZAZIE?@"#E8437B":@"#E73A8F"):UIColorMakeWithHex(@"#333333")} title:title action:action];
+    QMUIAlertAction *QMUIAction = [QMUIAlertAction actionWithTitle:title style:QMUIAlertActionStyleDefault handler:^(__kindof QMUIAlertController *aAlertController, QMUIAlertAction *qaction) {
+        if (action) {
+            action();
+        }
+    }];
+    [self.QMUIAlert addAction:QMUIAction];
 }
 
-- (void)addItemWithTitleAttributes:(NSDictionary *)attr title:(NSString *)title action:(dispatch_block_t)action {
-    QMUIButton *item = [QMUIButton buttonWithType:UIButtonTypeCustom];
-    item.titleLabel.font = [UIFont dynamic_fontWithName:IS_AZAZIE?OrdinaryBold:FunctionProDemi size:15.f];
-    if (attr) {
-        [item setAttributedTitle:[[NSAttributedString alloc] initWithString:title attributes:attr] forState:UIControlStateHighlighted];
-        [item setAttributedTitle:[[NSAttributedString alloc] initWithString:title attributes:attr] forState:UIControlStateNormal];
-    } else {
-        [item setTitleColor:UIColorMakeWithHex(@"#333333") forState:UIControlStateNormal];
-        [item setTitleColor:UIColorMakeWithHex(@"#333333") forState:UIControlStateHighlighted];
-        [item setTitle:title forState:UIControlStateNormal];
-        [item setTitle:title forState:UIControlStateHighlighted];
-    }
-    [self.items addObject:item];
-    [self drawFooterItemsIfNeeded];
-    @weakify(self);
-    [[item rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
-        @strongify(self);
-        [self dissmissWithAnimated:YES completion:^{
-            if (action) action();
-        }];
-    }];
-}
+//- (void)addItemWithTitleAttributes:(NSDictionary *)attr title:(NSString *)title action:(dispatch_block_t)action {
+//    QMUIButton *item = [QMUIButton buttonWithType:UIButtonTypeCustom];
+//    item.titleLabel.font = [UIFont dynamic_fontWithName:IS_AZAZIE?OrdinaryBold:FunctionProDemi size:15.f];
+//    if (attr) {
+//        [item setAttributedTitle:[[NSAttributedString alloc] initWithString:title attributes:attr] forState:UIControlStateHighlighted];
+//        [item setAttributedTitle:[[NSAttributedString alloc] initWithString:title attributes:attr] forState:UIControlStateNormal];
+//    } else {
+//        [item setTitleColor:UIColorMakeWithHex(@"#333333") forState:UIControlStateNormal];
+//        [item setTitleColor:UIColorMakeWithHex(@"#333333") forState:UIControlStateHighlighted];
+//        [item setTitle:title forState:UIControlStateNormal];
+//        [item setTitle:title forState:UIControlStateHighlighted];
+//    }
+//    [self.items addObject:item];
+//    [self drawFooterItemsIfNeeded];
+//    @weakify(self);
+//    [[item rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
+//        @strongify(self);
+//        [self dissmissWithAnimated:YES completion:^{
+//            if (action) action();
+//        }];
+//    }];
+//}
 
 - (void)show {
-    [self showWithAnimated:YES completion:NULL];
+    [self showWithAnimated:YES completion:NULL withoutMask:NO];
 }
 
 - (void)showWithoutMaskView {
@@ -217,55 +173,15 @@
     [self showWithAnimated:animated completion:complete withoutMask:NO];
 }
 
-- (void)showWithAnimated:(BOOL)animated completion:(dispatch_block_t)complete withoutMask:(BOOL)shouldMask{
-    if (self.hud) {
-        [self.hud hide];
-    }
-    self.hud = AZProgressHUD.hud.grace(0.5f).contentView(self.contentView).coverredWindow(YES).minContentSize(CGSizeMake(295, 195));
-    if (!animated) {
-        self.hud.displayAnimationType(AZProgressHUDAnimationTypeDefault);
-    }
+- (void)showWithAnimated:(BOOL)animated completion:(dispatch_block_t)complete withoutMask:(BOOL)shouldMask {
     if (shouldMask) {
-        self.hud.maskColor(UIColorMakeWithRGBA(0, 0, 0, 0.1));
+        UIView *maskView = [self.QMUIAlert valueForKey:@"maskView"];
+        maskView.backgroundColor = UIColorClear;
     }
-    [self.hud show];
-    [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
+    
+    [self.QMUIAlert showWithAnimated:animated];
+    
     if (complete) complete();
-}
-
-- (void)dissmissWithAnimated:(BOOL)animated completion:(dispatch_block_t)complete {
-    if (!animated) {
-        self.hud.hiddenAnimationType(AZProgressHUDAnimationTypeDefault);
-    }
-    [self.hud hide];
-    if (complete) complete();
-}
-
-- (void)drawFooterItemsIfNeeded {
-    NSUInteger count = self.items.count;
-    __block UIButton *lastButton = nil;
-    [self.items enumerateObjectsUsingBlock:^(QMUIButton * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        if (!obj.superview) {
-            [self.alertView addSubview:obj];
-        }
-        if (lastButton) {
-            [obj mas_remakeConstraints:^(MASConstraintMaker *make) {
-                make.leading.equalTo(lastButton.mas_trailing);
-                make.top.bottom.height.equalTo(lastButton);
-                make.width.equalTo(self.alertView).dividedBy(count);
-            }];
-            [obj drawSeparatorLineToPosition:UIRectEdgeLeft lineWidth:1];
-        } else {
-            [obj mas_remakeConstraints:^(MASConstraintMaker *make) {
-                make.leading.equalTo(self.alertView);
-                make.top.equalTo(self.footerSeparator.mas_bottom);
-                make.bottom.equalTo(self.alertView);
-                make.height.mas_equalTo(49);
-                make.width.equalTo(self.alertView).dividedBy(count);
-            }];
-        }
-        lastButton = obj;
-    }];
 }
 
 @end
